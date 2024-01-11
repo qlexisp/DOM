6ae5bf892d69f555530105716beea4fd099c4321
root <root@DESKTOP-LCHE35F>
2024-01-11 14:28:02 +0100

    fix

--- 07.DOM/script.js
+++
@@ -1,8 +0,0 @@
-console.log("Document Title", document.title);
-document.title = "Modifying the DOM";
-document.body.style.backgroundColor = "#E26EE5";
-
-const secondChild = document.body.children[1];
-for (const child of secondChild.children) {
-    console.log(child);
-}
--- 2.Selectors/script.js
+++
@@ -1,4 +0,0 @@
-const elementsimportants = querySelector(".important");
-for (const = element of elementsimportants) {
-    element.title = "This is an important item";
-}
